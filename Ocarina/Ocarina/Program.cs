6991c4e4e1e59ba66985cbedf57dafe77b4da0f4
Kitalpha <chris.reflection@gmail.com>
2017-10-13 21:34:03 +0100

    initial project

---
+++ Ocarina/Ocarina/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Ocarina
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var serviceProvider = new ServiceCollection()
+                .BuildServiceProvider();
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
